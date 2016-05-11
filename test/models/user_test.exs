ec1ad085b96c750f898fa216ebb5393f78c9e187
spicybreakfast <spicy@impossiblecreature.com>
2016-05-10 20:46:31 -0600

    update user model

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,11 @@ defmodule Peepchat.UserTest do
 
   alias Peepchat.User
 
-  @valid_attrs %{email: "some content", password_hash: "some content"}
+  @valid_attrs %{
+    email: "ted@example.com",
+    password: "abcdef11123345",
+    password_confirmation: "abcdef11123345"}
+
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -14,5 +18,25 @@ defmodule Peepchat.UserTest do
   test "changeset with invalid attributes" do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "mis-matched password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+     password: "1lh2bj1rjbk2",
+     password_confirmation: "b1bk23jkn12"})
+   refute changeset.valid?
+  end
+
+  test "missing password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+     password: "1lh2bj1rjbk2"})
+   refute changeset.valid?
+  end
+
+  test "short password doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+     password: "1lh2d",
+     password_confirmation: "1lh2d"})
+   refute changeset.valid?
   end
 end
